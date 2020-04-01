7f77dd5e4c14a6e53f9ee42f3d6fba958f1dea51
SaadAbbasi <saad.abbasi@venturedive.com>
2020-04-01 18:33:07 +0500

    fix typo

--- src/routes/auth/login.route.js
+++ src/routes/auth/login.route.js
@@ -6,10 +6,10 @@ const Utils = require('../../utils');
 router.post('/login', (request, response) => {
   userRepository
     .verify(request.body)
-    .then((foodie = {}) => {
+    .then((user = {}) => {
       const token = Utils.signJWT();
       response.setHeader('Authorization', token);
-      response.status(200).send(foodie);
+      response.status(200).send(user);
     })
     .catch((err) => void response.send(err.message));
 });
